a520f7457df6f04b17b0d6445310d9afc2892cce
msrsakib <sohansakib@gmail.com>
2020-01-05 21:57:00 +0600

    Pagination and static files added

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
-from abc import ABC
 
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.contrib.auth.mixins import (LoginRequiredMixin,
                                         UserPassesTestMixin
@@ -25,6 +25,19 @@ class PostListView(ListView):
     template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostCreateView(LoginRequiredMixin, CreateView):
